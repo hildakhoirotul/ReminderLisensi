893f3b79faeab574fa14b822e3400c5d18bbf875
hildakhoirotul <2041720161@student.polinema.ac.id>
2023-11-07 13:58:56 +0700

    revisi

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:license-reminders')->dailyAt('14.30');
+        $schedule->command('send:license-reminders')->dailyAt('14.00');
     }
 
     /**
